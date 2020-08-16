e21037b00860a9cdce148958f8e16efa830ff4ff
Kai <44775276+xyverious@users.noreply.github.com>
2020-08-17 06:22:40 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,20 +19,20 @@ bot.on("ready", () => {
   console.log("online")
   
 });
-
-client.on("message", async =>{
-  if (message.startsWith(prefix + 'ping')) {
-   try {
-    const m = await message.channel.send("Pinging..."); // Make sure the async is written, top of the client.on("message", ...)
-    const embed = new Discord.MessageEmbed()
+client.on('message', async message => {
+   if (message.startsWith(prefix + "ping")) {
+    try {
+      const m = await message.channel.send("Pinging..."); // Make sure the async is written, top of the client.on("message", ...)
+      const embed = new Discord.MessageEmbed()
       .setColor("RANDOM") // Tired of choosing the embed colors? Just type "RANDOM" on it!
       .addField("⌛ Latency", `**${m.createdTimestamp -  message.createdTimestamp}ms**`)
-      .addField("💓 API", `**${Math.floor(client.ws.ping)}ms**`) // Use "client.ping" if your Discord.js is < 1.15.1 --- Use "client.ws.ping" if your Discord.js is > 12.0.0
-    return m.edit(`🏓 Pong!`, embed);
-  } catch (error) {
-    return message.channel.send(`Something went wrong: ${error.message}`);
-    // Restart the bot as usual.
-  }
-} // easy way.
+      .addField("💓 HeartBreak", `**${Math.floor(client.ws.ping)}ms**`) // Use "client.ping" if your Discord.js is < 1.15.1 --- Use "client.ws.ping" if your Discord.js is > 12.0.0
+      return m.edit(`🏓 Pong!`, embed);
+    } catch (error) {
+      return message.channel.send(`Something went wrong: ${error.message}`);
+      // Restart the bot as usual.
+    }
+  } // easy way.
+});
 
 bot.login("NzM3MjgwNjQ0NTM4Njk1NzEy.Xx7EGw.6jfm5dKl5UtLX78SUs6NC7N-fWc")
