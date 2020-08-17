aa7b53ff34a607a2dd5f98fcbaea1fd0823adb0e
Kai <44775276+xyverious@users.noreply.github.com>
2020-08-17 13:08:24 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -59,6 +59,7 @@ bot.on('message', async message => {
       const m = await message.channel.send("Pinging..."); // Make sure the async is written, top of the client.on("message", ...)
       const embed = new Discord.MessageEmbed()
       .setColor("RANDOM") // Tired of choosing the embed colors? Just type "RANDOM" on it!
+      .setTimestamp(new Date())
       .addField("⌛ Latency", `**${m.createdTimestamp -  message.createdTimestamp}ms**`)
       .addField("💓 HeartBreak", `**${Math.floor(bot.ws.ping)}ms**`) // Use "client.ping" if your Discord.js is < 1.15.1 --- Use "client.ws.ping" if your Discord.js is > 12.0.0
       return m.edit(`🏓 Pong!`, embed);
